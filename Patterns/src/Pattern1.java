e50d84cff02e1b105426729e84e86d10e172fb8f
manoj010 <manojale100@gmail.com>
2023-03-09 20:31:05 +0545

    patterns

---
+++ Patterns/src/Pattern1.java
@@ -0,0 +1,11 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+            for (int j = 1; j <= 50; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ Patterns/src/Pattern3.java
@@ -0,0 +1,10 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
