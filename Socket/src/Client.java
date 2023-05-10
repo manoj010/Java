dc14df4a848ca73f5a8a38c19c040c7a414e88fa
manoj010 <manojale100@gmail.com>
2023-05-10 11:00:05 +0545

    Java-SocketProgramming

--- Socket/src/Client.java
+++ Socket/src/Client.java
@@ -1,10 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 1234);
+
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            while (true) {
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                System.out.println(bufferedReader.readLine());
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- Socket/src/Server.java
+++ Socket/src/Server.java
@@ -1,13 +1,38 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
+
+        boolean sending = true;
+
         System.out.println("Port 1234 is open for connection...");
         try {
+
             ServerSocket serverSocket = new ServerSocket(1234);
-            serverSocket.accept();
+            Socket socket = serverSocket.accept();
+
             System.out.println("A new client is connected");
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream, true);
+
+            Scanner scanner = new Scanner(System.in);
+
+            while(sending) {
+                System.out.println("Enter a message: ");
+                String message = scanner.nextLine();
+                if (message == "stop") {
+                    sending = false;
+                } else {
+                    printWriter.println(message);
+                }
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
